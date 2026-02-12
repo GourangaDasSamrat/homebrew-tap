c784bbabf8926eb22a1541e27028476e70778977
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-12 11:16:41 +0600

    Fix sha256 format in todo-cli.rb

--- Formula/todo-cli.rb
+++ Formula/todo-cli.rb
@@ -2,7 +2,7 @@ class TodoCli < Formula
   desc "A simple Todo CLI built in Go"
   homepage "https://github.com/GourangaDasSamrat/todo-cli-go"
   url "https://github.com/GourangaDasSamrat/todo-cli-go/releases/download/v1.0.0/todo-v1.0.0-darwin-amd64.tar.gz"
-  sha256 "sha256:02631fd71136a464c360b2d007f0fbe5b0f5b2273f45395168a064c1519042b9" # The action will update this automatically later
+  sha256 "02631fd71136a464c360b2d007f0fbe5b0f5b2273f45395168a064c1519042b9" # The action will update this automatically later
   license "MIT"
 
   def install
